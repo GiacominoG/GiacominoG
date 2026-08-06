9ed8471c2ec738ea5a1822bfdb426b007be96346
GiacominoG <giacominogiacomazzi@gmail.com>
2026-08-06 12:15:40 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let currentSlide = 0;
 const slideInterval = 5000; // Time in milliseconds (5 seconds)
 
 function nextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
 }
 
 setInterval(nextSlide, slideInterval);
